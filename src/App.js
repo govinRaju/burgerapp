e9fc2bf928ab98f6e0243a960f48862187c44cc1
Govind vuppu <vuppugovind3@gmail.com>
2020-04-20 00:10:13 -0500

    "removing old interceptors"

--- src/App.js
+++ src/App.js
@@ -4,10 +4,19 @@ import './App.css';
 import Layout from '../src/Components/Layout/Layout.js';
 import FarmBuilder from '../src/Containers/Agriculture/FarmBuilder';
 class App extends Component {
+  // state = {
+  //   show: true
+  // }
+  // componentDidMount() {
+  //   setTimeout(() => {
+  //     this.setState({ show: false });
+  //   }, 5000);
+  // }
   render() {
     return (
       <div className="App">
         <Layout >
+          {/* {this.state.show ? <FarmBuilder /> : null} */}
           <FarmBuilder />
         </Layout>
       </div>
