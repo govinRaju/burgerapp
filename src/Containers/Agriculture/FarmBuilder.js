53968d9dbcf3c1d13da89082b600216da42d7bde
Govind vuppu <vuppugovind3@gmail.com>
2020-05-04 12:17:29 -0500

    "orderSubmission &passing Databetween pages"

--- src/Containers/Agriculture/FarmBuilder.js
+++ src/Containers/Agriculture/FarmBuilder.js
@@ -94,33 +94,11 @@ class FarmBuilder extends Component {
         this.setState({ purchasing: false });
     }
     purchaseContinueHandler = () => {
-        // alert("purchase");
-        // this.setState({ loading: true });
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     customer: {
-        //         name: "Indian Farmer",
-        //         address: {
-        //             street: 'organicstreet1',
-        //             ZipCode: '533431',
-        //             country: 'India'
-        //         },
-        //         email: 'indianfarmer@farming.com'
-        //     },
-        //     deliveryMethod: 'fast'
-        // }
-        // axios.post('/orders.json', order)
-        //     .then(response => {
-        //         this.setState({ loading: false, purchasing: false })
-        //     })
-        //     .catch(error => {
-        //         this.setState({ loading: false, purchasing: false })
-        //     });
         const queryParams = [];
         for (let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
+        queryParams.push('price=' + this.state.totalPrice);
         const queryString = queryParams.join('&');
         this.props.history.push({
             pathname: '/checkout',
--- src/Containers/Checkout/ContactData/ContactData.js
+++ src/Containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './ContactData.css';
 import Button from '../../../Components/UI/Button/Button';
+import axios from '../../../axios-orders';
+import Spinner from '../../../Components/UI/Spinner/Spinner';
 class ContactData extends Component {
     state = {
         name: '',
@@ -8,19 +10,54 @@ class ContactData extends Component {
         address: {
             street: '',
             postalCode: ''
+        },
+        loading: false
+    }
+    orderHandler = (event) => {
+        event.preventDefault();
+        // console.log(this.props.ingredients);
+        // alert("purchase");s
+        this.setState({ loading: true });
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.props.price,
+            customer: {
+                name: "Indian Farmer",
+                address: {
+                    street: 'organicstreet1',
+                    ZipCode: '533431',
+                    country: 'India'
+                },
+                email: 'indianfarmer@farming.com'
+            },
+            deliveryMethod: 'fast'
         }
+        axios.post('/orders.json', order)
+            .then(response => {
+                this.setState({ loading: false, purchasing: false })
+                this.props.history.push('/');
+            })
+            .catch(error => {
+                this.setState({ loading: false, purchasing: false })
+            });
     }
     render() {
+        let form = (
+            <form>
+                <input type="text" name="name" placeholder="Your name" />
+                <input type="text" name="email" placeholder="Your email" />
+                <input type="text" name="street" placeholder="Street" />
+                <input type="text" name="postalCode" placeholder="ZipCode" />
+            </form>
+        );
+        if (this.state.loading) {
+            form = <Spinner />;
+        }
         return (
             <div className="ContactData">
                 <h4>Enter your contact Data</h4>
-                <form>
-                    <input type="text" name="name" placeholder="Your name" />
-                    <input type="text" name="email" placeholder="Your email" />
-                    <input type="text" name="street" placeholder="Street" />
-                    <input type="text" name="postalCode" placeholder="ZipCode" />
-                </form>
-                <Button btnType="Success">Estimate</Button>
+                {form}
+                <Button btnType="Success" clicked={this.orderHandler}>Estimate</Button>
             </div>
         )
     }
